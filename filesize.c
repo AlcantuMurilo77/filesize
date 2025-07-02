ede12353e71b8f8ac823aabad09d9f3f120e210e
Murilo <alcantumurilo@gmail.com>
2025-07-02 01:48:45 -0400

    Finish final touches

--- filesize.c
+++ filesize.c
@@ -2,12 +2,28 @@
 
 int main(int argc, char *argv[]) {
 	if (argc !=2){
-		printf("Command: %s filename\n", argv[0];
+		printf("Command: %s filename\n", argv[0]);
 		return 1;
 	}
 
+	FILE *file = fopen(argv[1], "rb");
+	if (!file) {
+		perror("Error opening file.");
+		return 1;
+	}
+
+	fseek(file, 0, SEEK_END);
+	long size = ftell(file);
+	fclose(file);
+
+	if (size == -1L) {
+		perror("Error obtaining file size.");
+		return 1;
+	}
+
+	printf("File size: %ld bytes\n", size);
 
 
 
-    return 0;
+	 return 0;
 }
